cd813deb389b2458e82f9861812322ba8b8600ed
Mariluz Congosto <mariluz.congosto@gmail.com>
2023-05-17 13:02:54 +0200

    Cambiados los límites del eje X de la grafica de rutinas

--- notebooks/share_functions_profiling_user.R
+++ notebooks/share_functions_profiling_user.R
@@ -41,67 +41,121 @@ Summary_tweets <- function(df) {
 # Functions shared by spread_tweet.Rmd
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
+# daily_routine_app
+#
+# Scatterplot chart de la rutina de publicación by app
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+daily_routine_app <- function(df, date_ini, date_end, time_zone) {
+  tweets_horario_df <- df %>% 
+    filter(date >= date_ini & date <= date_end) %>%
+    group_by(slot_time,hour_tweet,app) %>% 
+    summarise(
+      num_tweets = n(),
+      .group = "drop"
+    ) %>%
+    ungroup()
+  summary_tweets <- summary (df)
+  p <- ggplot() + 
+    geom_point(
+      data = tweets_horario_df,
+      aes(
+        x = hour_tweet,
+        y= slot_time,
+        size = num_tweets,
+        color = app 
+      ),
+      alpha =0.5) +
+    scale_y_datetime(
+      date_labels = format_time_plain(date_ini, date_end),
+      date_breaks = time_scale(date_ini, date_end)
+    ) +
+    scale_x_continuous(
+      breaks = seq(0,23,1),
+      limits= c(0,23),
+      sec.axis = dup_axis()
+    ) +
+    guides(color = guide_legend(nrow=2,override.aes = list(size = 4) ) ) +
+    labs(
+      title = paste0(base_title,": daily routine by app"),
+      subtitle = paste0("Time zone:", time_zone),
+      x = "",
+      y = "",
+      color = "",
+      size = "N. tweets") +
+    my_theme() +
+    theme(
+      panel.grid.major.x = element_line(),
+      legend.position="top"
+    )
+  return(p)
+}
+# Functions shared by spread_tweet.Rmd
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
 # daily_routine
 #
-# Scatterplot chart de la rutina de publicación
+# Scatterplot chart de la rutina de publicación by relation
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 daily_routine <- function(df, date_ini, date_end, time_zone) {
-tweets_horario_df <- df %>% 
-  filter(date >= date_ini & date <= date_end) %>%
-  group_by(slot_time,hour_tweet,relation_ext) %>%
-  summarise(
-    num_tweets = n(),
-    .group = "drop"
-  ) %>%
-  ungroup()
-summary_tweets <- summary (df)
-p <- ggplot() + 
-  geom_point(
-    data = tweets_horario_df,
-    aes(
-      x = hour_tweet,
-      y= slot_time,
-      size = num_tweets,
-      color = relation_ext
-    ),
-    alpha =0.5) +
-  scale_y_datetime(
-    date_labels = format_time_plain(date_ini, date_end),
-    date_breaks = time_scale(date_ini, date_end)
-  ) +
-  scale_x_continuous(
-    breaks = seq(0,23,1),
-    sec.axis = dup_axis()
-  ) +
-  guides(color = guide_legend(nrow=2,override.aes = list(size = 4) ) ) +
-  # Aplicamos color
-  scale_color_manual(
-    values = color_relation,
-    labels = paste(
-      "<span style='color:",
-      rev(color_relation),
-      "'>",
-      order_relation,
-      "(",
-      summary_tweets$percent,
-      "%)",
-      "</span>"), 
-    drop = FALSE
-  ) +
-  labs(
-    title = paste0(base_title,": dayly routine"),
-    subtitle = paste0("Time zone:", time_zone),
-    x = "",
-    y = "",
-    color = "",
-    size = "N. tweets") +
-  my_theme() +
-  theme(
-    panel.grid.major.x = element_line(),
-    legend.position="top",
-    legend.text=element_markdown(size=12)
-  )
+  df <- df %>%
+    filter(date >= date_ini & date <= date_end)
+  tweets_horario_df <- df %>% 
+    group_by(slot_time,hour_tweet,relation_ext) %>% 
+    summarise(
+      num_tweets = n(),
+      .group = "drop"
+    ) %>%
+    ungroup()
+  summary_tweets <- summary (df)
+  p <- ggplot() + 
+    geom_point(
+      data = tweets_horario_df,
+      aes(
+        x = hour_tweet,
+        y= slot_time,
+        size = num_tweets,
+        color = relation_ext
+      ),
+      alpha =0.5) +
+    scale_y_datetime(
+      date_labels = format_time_plain(date_ini, date_end),
+      date_breaks = time_scale(date_ini, date_end)
+    ) +
+    scale_x_continuous(
+      breaks = seq(0,23,1),
+      limits= c(0,23),
+      sec.axis = dup_axis()
+    ) +
+    guides(color = guide_legend(nrow=2,override.aes = list(size = 4) ) ) +
+    # Aplicamos color
+    scale_color_manual(
+      values = color_relation,
+      labels = paste(
+        "<span style='color:",
+        rev(color_relation),
+        "'>",
+        order_relation,
+        "(",
+        summary_tweets$percent,
+        "%)",
+        "</span>"), 
+      drop = FALSE
+    ) +
+    labs(
+      title = paste0(base_title,": daily routine"),
+      subtitle = paste0("Time zone:", time_zone),
+      x = "",
+      y = "",
+      color = "",
+      size = "N. tweets") +
+    my_theme() +
+    theme(
+      panel.grid.major.x = element_line(),
+      legend.position="top",
+      legend.text=element_markdown(size=12)
+    )
   return(p)
 }
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -112,8 +166,9 @@ p <- ggplot() +
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 daily_activity <- function(df, date_ini, date_end){
-  tweets_tipo_df <- tweets_df %>% 
-    filter(date >= date_ini & date <= date_end) %>%
+  df <- df %>%
+    filter(date >= date_ini & date <= date_end)
+  tweets_tipo_df <- df %>% 
     group_by(slot_time,relation_ext) %>%
     summarise(
       num_tweets = n(),
@@ -168,7 +223,7 @@ daily_activity <- function(df, date_ini, date_end){
       values = color_relation,
       labels = paste(
         "<span style='color:",
-        color_relation,
+        rev(color_relation),
         "'>",
         order_relation,
         "(",
@@ -189,6 +244,209 @@ daily_activity <- function(df, date_ini, date_end){
       legend.text=element_markdown(size=12)
     )
   return(p)
+}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# daily_activity_lang
+#
+# line chart desglosado por tipo de pubicación
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+daily_activity_lang <- function(df, date_ini, date_end){
+  max_langs <- 4
+  # Limitamos el tiempo
+  df <- df %>%
+    filter(date >= date_ini & date <= date_end)
+  # Calculamos el top de lenguajes
+  top_lang_df <- df %>%
+    # Agrupamos por lenguaje y contamos cuantos tweets hay en cada uno de ellos
+    group_by(lang) %>%
+    summarise(
+      num_tweets = n(),
+      .groups ="drop") %>%
+    ungroup() %>%
+    arrange(desc( num_tweets))
+  tweets_lang_df <- df %>% 
+    group_by(slot_time,lang) %>%
+    summarise(
+      num_tweets = n(),
+      .groups = "drop"
+    ) %>%
+    ungroup()
+  #ordenamos los lenguajes de más a menor frecuencia
+  order_langs <- unique(top_lang_df$lang %>% head(max_langs))
+  tweets_lang_df$lang <- factor(tweets_lang_df$lang, levels = order_langs)
+  tweets_tipo_tot_df <- tweets_df %>% 
+    filter(date >= date_ini & date <= date_end) %>%
+    group_by(slot_time) %>%
+    summarise(
+      num_tweets = n(),
+      .groups = "drop"
+    ) %>%
+    ungroup()
+  max_tweets <- max(tweets_tipo_tot_df$num_tweets,na.rm = TRUE)
+  summary_tweets <- summary_lang (df, max_langs)
+  # Definimos los colores de los lenguajes por defecto
+  color_lang <- c(
+    "es" = "#FF0000",
+    "ca" = "#FFFF00",
+    "eu" = "#5EFF2E",
+    "en" = "#19C3FF",
+    "fr" = "#0070C0",
+    "de" = "#005426",
+    "pt" = "#FF9933",
+    "it" = "#00B050",
+    "und" = "#808080",
+    "qme" = "#808080",
+    "zxx" = "#808080",
+    "qht" = "#808080"
+  )
+  p <- ggplot() + 
+    geom_col(
+      data = tweets_lang_df, 
+      aes(
+        x = slot_time,
+        y = num_tweets,
+        fill = lang
+      ),
+      size =1, alpha  = 0.7)+
+    geom_text_repel(
+      data = tweets_tipo_tot_df %>%   
+        top_n(2, num_tweets),
+      aes(
+        x = slot_time,
+        y = num_tweets * 1.1, 
+        label = paste(slot_time,"\n(",num_tweets," tweets)")
+      ),
+      force = 10,
+      max.time = 10,
+      color = "grey50",
+      size = 3.5,
+      vjust = .5,
+      segment.colour = "grey80",
+      show.legend = FALSE
+    ) +
+    scale_x_datetime(
+      date_labels = format_time(date_ini, date_end),
+      date_breaks = time_scale(date_ini, date_end)
+    ) +
+    scale_y_continuous(
+      labels = label_number_si(),
+      limits= c(0,max_tweets*1.5),
+      expan =c(0,0)
+    ) +
+    scale_fill_manual(
+      values = color_lang,
+      labels = paste(
+        "<span style='color:",
+        rev(color_lang),
+        "'>",
+        order_langs,
+        "(",
+        summary_tweets$percent,
+        "%)",
+        "</span>"), 
+      drop = FALSE
+    ) +
+    labs(
+      title = paste0(base_title,": Tipo de tweets"),
+      x = "",
+      y = "Num. Tweets per day",
+      fill=""
+    ) +
+    my_theme() +
+    theme(
+      legend.position="top",
+      legend.text=element_markdown(size=12)
+    )
+  return(p)
+}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# topics_tweets
+#
+# line chart de doble escala con los tweet originales vs impacto
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+topics_tweets <- function(df, date_ini, date_end, topics){
+
+  custom_stop_words <- bind_rows(
+    stop_words,
+    data_frame(word = tm::stopwords("spanish"),lexicon = "custom"),
+    data_frame(word = tm::stopwords("catalan"),lexicon = "custom")
+  )
+  # extraemos los topics
+  topics_df <- tweets_df %>%
+    filter(date >= date_ini & date <= date_end) %>%
+    mutate(text_plain = gsub('http\\S+\\s*',"",text)) %>% # Quitamos las URLs
+    mutate(text_plain = gsub("RT @\\w+:","",text_plain)) %>% # Quitamos los RTs
+    mutate(text_plain = gsub("&amp;","&",text_plain)) %>% # Rectificamos el &
+    mutate(text_plain = gsub("@\\w+","",text_plain)) %>% # Quitamos las menciones
+    select(slot_time, text_plain) %>%
+    unnest_tokens(word, text_plain) %>% # Convertimos las frases en un conjunto de palabras
+    anti_join(custom_stop_words) %>% # Eliminamos las stop words
+    filter (word %in% topics) %>%
+    group_by(word, slot_time) %>%
+    summarise(nun_topics = n()) %>%
+    mutate(cumulative_sum = cumsum(nun_topics))
+  min_date <- min(topics_df$slot_time)
+  max_date <- max(topics_df$slot_time)
+  visible_dates <- as.POSIXct(seq(min_date,max_date, by = time_scale(date_ini, date_end)) )
+  limit_x = as.POSIXct(c(min_date, max_date+( expand_time(date_ini, date_end, 50))))
+  limit_y =  max(topics_df$cumulative_sum) 
+  p <- ggplot() + 
+    geom_line(
+      data = topics_df, 
+      aes(
+        x = slot_time,
+        y = cumulative_sum,
+        color = word
+      ),
+      show.legend =FALSE,
+      size =1, alpha  = 0.7)+
+    geom_text_repel(
+      data = topics_df %>% 
+        top_n(1, cumulative_sum),
+      aes(
+        x = slot_time, y = cumulative_sum, color = word,
+        label = paste0(
+          word,
+          "(",
+          format(cumulative_sum, big.mark=".",decimal.mark=","),
+          " ref.)")
+      ), 
+      vjust = 1,
+      size = 4,
+      nudge_x =  expand_time(min_date,max_date, 10), # Ajuste eje x
+      nudge_y = 0.005,  # Ajuste eje y
+      direction = "y",
+      max.overlaps = 30,
+      segment.size = 0.5,
+      segment.linetype = 2,
+      show.legend =FALSE
+    )+
+    scale_x_datetime(
+      limits=limit_x,
+      breaks = visible_dates,
+      date_labels = format_time(date_ini, date_end)
+    ) +
+    scale_y_continuous(
+      labels = label_number_si(),
+      limits= c(0,limit_y*1.3 ),
+      expand= c(0,0)
+    ) +
+    labs(
+      title = paste0(base_title,": Topics"),
+      x = "",
+      y = "Num. accumulated topics per day",
+      color = ""
+    ) +
+    my_theme() +
+    theme(
+      legend.position="top"
+    )
+  return (p)
 }
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
@@ -472,6 +730,64 @@ retweets_frequency <- function(df, date_ini, date_end){
     # Definimos el título principal y el de los ejes
     labs(
       title = paste0(base_title,": Most frequent retweets"),
+      subtitle = paste0(
+        "Period: ",
+        as.Date(date_ini,"%Y-%m-%d"),
+        " - ",
+        as.Date(date_end,"%Y-%m-%d")
+      ),
+      x = "",
+      y = "",
+      color=""
+    ) +
+    # Aplicamos un template minimalista
+    my_theme() +
+    theme ( panel.border = element_blank())
+  return(p)
+}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# reply_frequency
+#
+# Wordcloud con los términos más frecuentes
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+reply_frequency <- function(df, date_ini, date_end){
+  corpus_menciones <- df %>%
+    filter(date >= date_ini & date <= date_end) %>%
+    # Extraemos los handles de los comentarios con una expresión regular "@\\w+"
+    mutate(mentions = ifelse(relation == "reply",str_extract(text,"@\\w+"), NA)) %>%
+    # Eliminamos las filas que están vacías 
+    filter(!is.na(mentions)) %>%
+    select(mentions) %>%  # Seleccionamos las menciones y periodo
+    group_by(mentions) %>%
+    summarise(
+      freq = n(),
+      .groups = "drop"
+    ) %>% # Calculamos la frecuencia de cada palabra
+    ungroup() %>%
+    arrange(desc(freq)) %>% # Ordenamos de mayor a menor frecuencia de aparición
+    head(30)
+  paleta <- brewer.pal(8, "Dark2")
+  p <- ggplot() +
+    # Dibujamos la nube de palabras
+    geom_text_wordcloud_area(
+      data = corpus_menciones,
+      aes(
+        label = mentions,
+        size = freq,
+        color = freq
+      ),
+      angle = 0.35) +
+    # Definimos la proporción del tamaño de las letras según frecuencia
+    geom_text_wordcloud_area(rm_outside = TRUE) +
+    geom_text_wordcloud_area(area_corr_power = 1) +
+    scale_radius(range = c(4, 20), limits = c(0, NA)) +
+    # Aplicamos una paleta de color
+    scale_color_gradientn(colors = paleta) +
+    # Definimos el título principal y el de los ejes
+    labs(
+      title = paste0(base_title,": Most frequent replies"),
       subtitle = paste0(
         "Period: ",
         as.Date(date_ini,"%Y-%m-%d"),
